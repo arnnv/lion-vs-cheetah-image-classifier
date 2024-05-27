f428a8ca1c9260a08222d510f66096be1043ef3b
Arnav <arnnv.g@gmail.com>
2024-05-27 15:17:24 +0530

    Update requirements.txt

--- script.py
+++ script.py
@@ -1,13 +1,19 @@
+# %%
+# DATASET LINK: https://www.kaggle.com/datasets/mikoajfish99/lions-or-cheetahs-image-classification
+
+# %%
 import os
 import random
 import shutil
-from shutil import copyfile
 import numpy as np
 import tensorflow as tf
+from shutil import copyfile
+import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import InceptionV3
 from tensorflow.keras.optimizers import RMSprop
 
+# %%
 root_dir = './data/'
 
 if os.path.exists(root_dir):
@@ -21,6 +27,7 @@ def create_train_val_dirs(root_dir):
 
 create_train_val_dirs(root_dir)
 
+# %%
 def print_subdirectories(root_dir):
     for root, dirs, files in os.walk(root_dir):
         for name in dirs:
@@ -28,8 +35,11 @@ def print_subdirectories(root_dir):
 
 print_subdirectories(root_dir)
 
+# %%
 def split_data(SOURCE_DIR, TRAIN_DIR, VALIDATION_DIR, SPLIT_SIZE):
+
     all_files = []
+
     for file in os.listdir(SOURCE_DIR):
         if os.path.getsize(os.path.join(SOURCE_DIR, file)) > 0:
             all_files.append(file)
@@ -37,7 +47,9 @@ def split_data(SOURCE_DIR, TRAIN_DIR, VALIDATION_DIR, SPLIT_SIZE):
             print(f'{file} is zero length, so ignoring')
     
     all_files = random.sample(all_files, len(all_files))
+
     SPLIT_POINT = int(len(all_files)*SPLIT_SIZE)
+
     train_files = all_files[:SPLIT_POINT]
     validation_files = all_files[SPLIT_POINT:]
 
@@ -51,12 +63,16 @@ def split_data(SOURCE_DIR, TRAIN_DIR, VALIDATION_DIR, SPLIT_SIZE):
         dest = os.path.join(VALIDATION_DIR, file)
         copyfile(src, dest)
 
+# %%
 CHEETAH_SOURCE_DIR = './images/Cheetahs'
 LION_SOURCE_DIR = './images/Lions'
+
 CHEETAH_TRAIN_DIR = './data/training/cheetahs'
 LION_TRAIN_DIR = './data/training/lions'
+
 CHEETAH_VALIDATION_DIR = './data/validation/cheetahs'
 LION_VALIDATION_DIR = './data/validation/lions'
+
 split_size = 0.9
 
 split_data(CHEETAH_SOURCE_DIR, CHEETAH_TRAIN_DIR, CHEETAH_VALIDATION_DIR, split_size)
@@ -64,12 +80,15 @@ split_data(LION_SOURCE_DIR, LION_TRAIN_DIR, LION_VALIDATION_DIR, split_size)
 
 print(f'Original cheetah images: {len(os.listdir(CHEETAH_SOURCE_DIR))}')
 print(f'Original lion images: {len(os.listdir(LION_SOURCE_DIR))}')
+print('\n\n')
 print(f'Training cheetah images: {len(os.listdir(CHEETAH_TRAIN_DIR))}')
 print(f'Training lion images: {len(os.listdir(LION_TRAIN_DIR))}')
 print(f'Validation cheetah images: {len(os.listdir(CHEETAH_VALIDATION_DIR))}')
 print(f'Validation lion images: {len(os.listdir(LION_VALIDATION_DIR))}')
 
+# %%
 def train_val_gens(TRAINING_DIR, VALIDATION_DIR):
+
     train_datagen = ImageDataGenerator(
         rescale=1./255,
         rotation_range=40,
@@ -99,9 +118,12 @@ def train_val_gens(TRAINING_DIR, VALIDATION_DIR):
 
     return train_generator, validation_generator
 
+# %%
 train_generator, validation_generator = train_val_gens('./data/training', './data/validation')
 
+# %%
 def create_model():
+
     pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
                                     include_top=False,
                                     weights='imagenet')
@@ -127,6 +149,7 @@ def create_model():
 
     return model
 
+# %%
 model = create_model()
 
 history = model.fit(train_generator,
@@ -134,3 +157,33 @@ history = model.fit(train_generator,
                     validation_data=validation_generator,
                     verbose=1
                     )
+
+# %%
+train_acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+train_loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(train_acc))
+
+plt.figure(figsize=(8, 3))
+
+plt.subplot(1,2,1)
+plt.plot(epochs, train_acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.subplot(1,2,2)
+plt.plot(epochs, train_loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.tight_layout()
+
+# %%
+
+
+
